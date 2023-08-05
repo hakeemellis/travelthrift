46b2c5b806d10d7eea1b183ed4b98f101bf4214f
Hakeem Ellis <114674803+hakeemellis@users.noreply.github.com>
2023-08-05 13:14:16 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,161 +1,83 @@
-// Nav Bar
+window.addEventListener('load', async () => {
+  const locationInput = document.getElementById('locationInput');
+  const checkinInput = document.getElementById('checkinInput');
+  const checkoutInput = document.getElementById('checkoutInput');
+  const searchButton = document.getElementById('searchButton');
+  const countrySelect = document.getElementById('countrySelect');
 
-$(".home").html("Home");
-$(".service").html("The Service");
-$(".service1").html("How It Works");
-$(".service2").html("How A Mac Could Help You");
-$(".service3").html("Payment Plans");
-$(".service4").html("Login");
-$(".us").html("About Us");
-$(".choose").html("Why Choose Us");
-$(".testimonials").html("Testimonials");
-$(".oteam").html("Our Team");
-$(".contact").html("Contact Us");
-$(".account").html("Account");
+  // Fetch the list of country names from a REST API
+  const countriesUrl = 'https://restcountries.com/v2/all?fields=name';
+  const response = await fetch(countriesUrl);
+  const countries = await response.json();
 
+  // Populate the dropdown list with the country names
+  countries.forEach(country => {
+    const option = document.createElement('option');
+    option.value = country.name;
+    option.text = country.name;
+    countrySelect.appendChild(option);
+  });
 
-// Index Body
+  searchButton.addEventListener('click', async () => {
 
-$(".h3").html("Stay Up-to-Date Easy with the");
-$(".arial").html("<b>Latest MacBooks from Apple</b>");
-$("#indexbody").html("Get access to a Macbook Air/Pro for free as a trial or rent one for an extended period");
-$("#indexbody").append("<br><br><br>with no obligation to buy. This gives our users complete access to the latest and greatest");
-$("#indexbody").append("<br><br><br>from Apple, utilizing the latest MacOS and computer without needing to pay to own.");
-$(".register").html("REGISTER NOW");
-$(".signin").html("SIGN IN");
+    const apiKey = '3f62a0a69amsh1a88001e18de088p19ac4ajsn3529f7e09f40';
+    const location = locationInput.value;
+    const checkinDate = checkinInput.value;
+    const checkoutDate = checkoutInput.value;
+    const countryName = countrySelect.value;
 
-// Footer
+    const url = `https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations?name=${location}&search_type=HOTEL`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-RapidAPI-Key': apiKey,
+        'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+      }
+    });
 
-$(".fcontact").html("support@maclove.com</a> <b>|</b> (800)-224-2101");
-
-
-// Login
-
-$(".login").html("Login");
-$(".fpassword").html("Forget Password?");
-$(".noaccount").prepend("Don't have an account?");
-
-
-// Signup
-
-$(".signup").html("Sign Up");
-$(".haccount").prepend("Have an Account?");
-$(".login1").append("Login Now");
-
-// Forget Password
-
-$(".fpass").html("Forget Password");
-$(".ftext").html("Enter your email address and we'll send you an email with instructions to reset your password.");
-$(".ftext1").html("Email request has been sent. Please check your inbox to follow the instructions stated.");
-$(".ftext2").prepend("Suddenly remember it?");
-$(".rpass").html("Reset Password");
-$(".aroute").prepend("Alternate Route?");
-$(".bth").prepend("Back to Home");
-$(".btl").prepend("Back to Login");
-
-
-// Account Page
-
-$(".apage").html("Account Page");
-$(".morders").html("My Orders");
-$(".thome").html("To Home");
-$(".adetails").html("<strong>Full Name :</strong> James Raymond");
-$(".adetails").append("<br><strong>Email :</strong> user@email.com");
-$(".adetails").append("<br><strong>Location :</strong> USA");
-
-// Show Password
-
-$(".spass").append(" Show Password");
-
-// Spinner Load
-
-const preloaderWrapper = document.querySelector('.preloader-wrapper');
-
-window.addEventListener('load', function() {
-    preloaderWrapper.classList.add('fade-out-animation');
-});
-
-
-// Password Match //
-
-const form = document.getElementById("myForm");
-const password = form.password;
-const confirmPassword = form.confirmPassword
-const feedback = document.getElementById("confirmPassword-feedback");
-let isPasswordMatch = false;
-
-confirmPassword.addEventListener("input",() => {
-    if (password.value != confirmPassword.value) {
-        feedback.innerHTML = "Password doesn't match";
-        isPasswordMatch = false
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    else {
-        feedback.innerHTML = "";
-        isPasswordMatch = true;
-    }
-});
+    const data = await response.json();
+    console.log(data);
 
-form.onsubmit = function () {
-    if(isPasswordMatch) {
-        alert ("Thanks for Successfully Signing up with MacLove");
-        return true;
-    }
+    const matchingItems = data.filter(item => item.cityName.toLowerCase() === location.toLowerCase());
 
-    alert ("Wait! Check your password again. It's not matching");
-    return false;
-};
-
-// Additional Password Match Code //
-
-function verifyPassword() {  
-    var pw = document.getElementById("password").value;  
-     
-   //minimum password length validation  
-    if(pw.length < 8) {  
-       document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
-       return false;  
-    }  
-    
-  //maximum length of password validation  
-    if(pw.length > 15) {  
-       document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";  
-       return false;  
-    } else {  
-       alert("Password is correct");  
-    }  
-  }  
-
-// Show or Hide Password //
-
-function myFunction() {
-    var pass = document.getElementById("password");
-    if (pass.type === "password") {
-      pass.type = "text";
+    let cityId = '';
+    if (matchingItems.length === 1) {
+      if (matchingItems[0].countryName.toLowerCase() === countryName.toLowerCase()) {
+        cityId = matchingItems[0].cityId;
+      } else {
+        alert('City not found in specified country. Please try again.');
+        return;
+      }
+    } else if (matchingItems.length > 1) {
+      const matchingItem = matchingItems.find(item => item.countryName.toLowerCase() === countryName.toLowerCase());
+      if (matchingItem) {
+        cityId = matchingItem.cityId;
+      } else {
+        alert('City not found in specified country. Please try again.');
+        return;
+      }
     } else {
-      pass.type = "password";
+      alert('City not found. Please try again.');
+      return;
     }
-  }
 
-  function myFunction1() {
-    var pass = document.getElementById("confirmpassword");
-    if (pass.type === "password") {
-      pass.type = "text";
-    } else {
-      pass.type = "password";
-    }
-  }
+    const itemList = data.map(item => {
+      if (item.cityName.toLowerCase() === location.toLowerCase() && item.countryName.toLowerCase() === countryName.toLowerCase()) {
+        return { itemName: item.itemName, address: item.address, id: item.id, cityID: item.cityID, provinceName: item.provinceName, country: item.country };
+      }
+    }).filter(item => item);
 
-  // Text Color
+    sessionStorage.setItem('cityId', cityId);
+    sessionStorage.setItem('itemList', JSON.stringify(itemList));
+    sessionStorage.setItem('location', location);
+    sessionStorage.setItem('checkinDate', checkinDate);
+    sessionStorage.setItem('checkoutDate', checkoutDate);
 
-  document.body.style.color = "black";
-
-  //Applying style to footer
-const footer = document.querySelector("footer");
-{ footer.style.backgroundColor ="white";}
-
-
-
-
-
-
+    window.location.href = 'hotelsearchresults.html';
+  });
+});
